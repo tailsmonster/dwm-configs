29f8a49c50466fc6b7f21816a68ccdb14ddc0716
Nico <aroca.twelve@proton.me>
2024-11-22 06:23:08 -0500

    updated fonts

--- dwm/config.h
+++ dwm/config.h
@@ -10,8 +10,12 @@ static const unsigned int gappx     = 6;        /* gaps between windows*/
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=16" };
-static const char dmenufont[]       = "monospace:size=16";
+static const int horizpadbar        = 7;        /* horizontal padding for statusbar */
+static const int vertpadbar         = 7;        /* vertical padding for statusbar */
+static const char *fonts[]          = { "Firacode:weight=bold:size=14:antialias=true:hinting=true",
+                                  		"Hack:size=14:antialias=true:autohint=true",
+                                 		"JoyPixels:size=16:antialias=true:autohint=true"};
+static const char dmenufont[]       = "Firacode:weight=bold:size=14:antialias=true:hinting=true";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -25,6 +29,7 @@ static const char *colors[][3]      = {
 
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+/* static const char *tags[] = { "", "", "", "", "", "", "", "", "" }; */
 
 static const Rule rules[] = {
 	/* xprop(1):

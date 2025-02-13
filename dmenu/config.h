467b4599cdc58c005a7c716b793b0780434c9271
tailsmonster <aroca.twelve@proton.me>
2025-02-13 16:26:51 -0500

    more pink

--- dmenu/config.h
+++ dmenu/config.h
@@ -5,17 +5,17 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 static int fuzzy  = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"Firacode:weight=bold:size=12"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeSelHighlight] = { "#ffc978", "#005577" },
+	[SchemeSel] = { "#eeeeee", "#c264f5" },
+	[SchemeSelHighlight] = { "#ffc978", "#c264f5" },
 	[SchemeNormHighlight] = { "#ffc978", "#222222" },
 	[SchemeOut] = { "#000000", "#00ffff" },
-	[SchemeMid] = { "#eeeeee", "#004139" },
+	[SchemeMid] = { "#eeeeee", "#410026" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
